44377e407577561a04364f7a4f1d997c1447fd36
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2022-10-22 19:16:38 +0700

    move to deprecated branch

--- app.js
+++
@@ -1,6 +0,0 @@
-const discordBot = require('./discordClient');
-
-(async () => {
-  const discordClient = discordBot.new();
-  discordClient.doLogin();
-})();
--- helpers/current-time.js
+++
@@ -1,3 +0,0 @@
-const moment = require("moment");
-
-module.exports = () => moment().format('DD/MM/YYYY | HH:mm:ss');
