eea67e2634919e0bc742f123a452109dae12dbd5
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2022-11-09 10:05:20 +0700

    feat: add discord bot status and activity

--- src/discord-bot/events/ready.js
+++ src/discord-bot/events/ready.js
@@ -1,8 +1,23 @@
+const { ActivityType } = require('discord.js');
 const { steamBots } = require('../../steam-bot');
 const SteamBot = require('../../steam-bot/steam-bot');
 const SteamAccount = require('../../services/steam-account.service');
 
 const { logger } = require('../../utils/logger');
+
+const setBotStatus = (client) => {
+  client.user.setPresence({
+    status: 'online',
+    activities: [
+      {
+        name: '/help',
+        type: ActivityType.Listening,
+      },
+    ],
+  });
+
+  logger.info('Bot status updated.');
+};
 
 module.exports = {
   name: 'ready',
@@ -10,6 +25,12 @@ module.exports = {
   async execute(client) {
     try {
       logger.info(`Bot Ready! Logged in as ${client.user.tag}`);
+      setBotStatus(client);
+
+      // Set bot status every 10 minutes
+      setInterval(() => {
+        setBotStatus(client);
+      }, 10 * 60 * 1000);
 
       // * Restart all Steam accounts that are running before
       const steamAccounts = await SteamAccount.getAllRunning();
