8eb2e62c0d0cb0722e928db73302597d5649de69
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2022-11-09 13:57:03 +0700

    refactor: force logger to use Asia/Jakarta timezone

--- src/startup/logging.js
+++ src/startup/logging.js
@@ -1,14 +1,31 @@
 const { loggers, format, transports } = require('winston');
 const { combine, timestamp, printf } = format;
 
-module.exports = () => {
-  const loggerFormat = printf(({ level, message, timestamp: ts }) => `[${ts} - ${level.toUpperCase()}] ${message}`);
+const loggerFormat = printf(({ level, message, timestamp: ts }) => `[${ts} - ${level.toUpperCase()}] ${message}`);
 
+const timezoned = () => {
+  const datetime = new Date().toLocaleString('en-US', {
+    timeZone: 'Asia/Jakarta',
+    hour12: false,
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short',
+  });
+
+  // Format to YYYY-MM-DD HH:mm:ss timezone
+  return datetime.replace(/(\d+)\/(\d+)\/(\d+), (\d+):(\d+):(\d+) (.+)/, '$3-$1-$2 $4:$5:$6 $7');
+};
+
+module.exports = () => {
   loggers.add('default', {
     level: 'info',
     exitOnError: true,
     format: combine(
-      timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      timestamp({ format: timezoned }),
       loggerFormat,
     ),
     transports: [
