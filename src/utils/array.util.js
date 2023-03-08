b67bf538927d6f1a737391684ee3ccd4ae73518f
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2023-03-08 18:08:17 +0700

    refactor: add chunk array util and move array utils into one file

---
+++ src/utils/array.util.js
@@ -0,0 +1,24 @@
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+};
+
+// Split an array into smaller arrays of given size
+const chunkArray = (array, size) => {
+  const chunks = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+
+  return chunks;
+};
+
+module.exports = {
+  shuffleArray,
+  chunkArray,
+};
